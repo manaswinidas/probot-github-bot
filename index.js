55772bb866730a56533933d28acdad05ad5c8218
manaswinidas <dasmanaswini10@gmail.com>
2024-05-14 11:40:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Probot} app
+ */
+export default (app) => {
+  // Your code here
+  app.log.info("Yay, the app was loaded!");
+
+  app.on("issues.opened", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!",
+    });
+    return context.octokit.issues.createComment(issueComment);
+  });
+
+  app.on("pull_request.opened", async (context) => {
+    let { reviewers } = await context.config("auto_assign.yml");
+    reviewers = reviewers.filter((reviewer) => reviewer !== "manaswinidas");
+
+    const params = context.pullRequest({ reviewers });
+
+    return context.octokit.pulls.requestReviewers(params);
+  });
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+};
